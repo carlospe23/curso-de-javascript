3695c310d72c70ddf1bed1e60f70fbbf1f0ca0b2
Carlos Reyes <careyespe26@gmail.com>
2023-02-03 02:30:24 -0500

    curso de JavaScript

---
+++ JUNIOR/app.js
@@ -0,0 +1,22 @@
+// VARIABLES
+
+variable1 = 'texto o str'; //texto o cadena de texto
+variable2 = 2; //numerico 
+variable3 = true; //bool o buleano
+
+
+// VAR LET Y CONST
+
+var numero = 12; // var tiene alcance global y se puede re definir
+let numero2 = 13; // let es de alcance en donde sea declarada y solo se puede re asignar
+const numero3 = 14; // no puede cambiar su valor, es constante
+
+// hoisting que es
+// hoisting: las funciones y variables se van al inicio del programa cuando se compila
+
+
+let nombre = prompt("dime tu nombre: "); //pedimos datos en una alerta y se almacena en la variable
+alert(nombre) // mostramos el dato que medimos
+
+document.write("Hola " + nombre) // Escribimos sobre el documento html
+
